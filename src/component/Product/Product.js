0de0ea0776098b816eb3edcecf3cde235dddaeb5
Your Name <you@example.com>
2021-03-28 00:19:04 +0600

    all done

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
 
 const Product = (props) => {
-    console.log(props.product.name)
+    console.log(props)
+    const { img, name, seller, price, stock } = props.product;
     return (
-        <div>
-            <h4>{props.product.name}</h4>
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <br />
+                <p><small>by : {seller}</small></p>
+                <p>${price}</p>
+                <br />
+                <p><small>Only {stock} left in stock - order soon</small></p>
+                <button className="main-button"
+                onClick={()=>props.handleAddProduct(props.product)}
+                >
+                    <FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
+            </div>
+
         </div>
     );
 };
