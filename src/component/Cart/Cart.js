8d5e1946e1c707d9db9374faf52d1123278b74e0
Your Name <you@example.com>
2021-04-02 21:41:59 +0600

    done

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Cart = (props) => {
     const cart = props.cart;
-    console.log(cart);
+    // console.log(cart);
     const total = cart.reduce((total,prd) => total + prd.price, 0);
     let shipping = 0;
     if(total > 35){
@@ -24,12 +25,17 @@ const Cart = (props) => {
     
     return (
         <div>
-            <h4>Order Summary</h4>
+            <h4 className="text-primary">Order Summary</h4>
             <p>Items Ordered : {cart.length}</p>
             <p>Product price : {formatNumber(total)}</p>
             <p><small>Shipping Cost : {shipping}</small></p>
             <p><small>Tax + Vat : {tax}</small></p>
             <p>Total Price : {grandTotal}</p>
+            <br/>
+            <Link to="/review">
+                <button className="main-button">Review Order</button>
+            </Link>
+           
         </div>
     );
 };
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -6,8 +6,8 @@ const Header = () => {
         <div className="header">
             <img src={logo} alt=""/>
             <nav><a href="/shop">Shop</a>
-            <a href="/order">Order Review</a>
-            <a href="/manage">Manage Inventory</a></nav>
+            <a href="/review">Order Review</a>
+            <a href="/inventory">Manage Inventory</a></nav>
         </div>
     );
 };
